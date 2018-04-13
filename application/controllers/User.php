b1679c90fbaab72ae41bb9273164f87e4ce85549
fikrius <fikrius@gmail.com>
2018-04-13 22:01:23 +0700

    load home, di controller user

--- application/controllers/User.php
+++ application/controllers/User.php
@@ -3,5 +3,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class User extends CI_Controller{
 
+	public function index(){
+		$this->load->view('User/pages/templates/header');
+		$this->load->view('User/pages/home');
+	}
 
 }
